b5401f827aefb54f11bccd70f63b8b3346dfe4cb
StefaniaDev <stefania.zinno@unina.it>
2021-02-12 15:09:46 +0100

    First Commit

---
+++ Shared/RestaurantMenuAdApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMenuAdApp.swift
+//  Shared
+//
+//  Created by Stefania Zinno on 09/02/21.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMenuAdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
